0a4e2cc23e1898aea81f2e5a975630ad781dfc31
tulshina <yunchenkotanya@gmail.com>
2018-02-12 23:43:26 +0300

    Класссы и конструкторы

---
+++ sandbox_new/src/main/java/ru/stqa/pft/sandbox_new/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox_new;
+
+public class Rectangle {
+
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+}
+
---
+++ sandbox_new/src/main/java/ru/stqa/pft/sandbox_new/Square.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox_new;
+
+public class Square {
+
+    public double l;
+    public Square(double l) {
+        this.l = l;
+    }
+}
